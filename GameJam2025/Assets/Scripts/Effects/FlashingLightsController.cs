6a584ca2c4a75456a29bc19b45b861223865063c
LilboX3 <lilligruber02@gmail.com>
2025-08-01 18:16:48 +0200

    added script to make lights flicker

--- GameJam2025/Assets/Scripts/Effects/FlashingLightsController.cs
+++ GameJam2025/Assets/Scripts/Effects/FlashingLightsController.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class FlashingLightsController : MonoBehaviour
 {
-    private List<GameObject> lights;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<Light2D> neonLights;
+    public float minOnTime = 0.2f;
+    public float maxOnTime = 1.2f;
+    public float minOffTime = 0.3f;
+    public float maxOffTime = 2.0f;
+
     void Start()
     {
-        lights = GetComponentInChildren<GameObject>();
+        foreach (Light2D light in neonLights)
+        {
+            StartCoroutine(FlickerRoutine(light));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FlickerRoutine(Light2D light)
     {
-        
+        while (true)
+        {
+            float onDelay = Random.Range(minOffTime, maxOffTime);
+            yield return new WaitForSeconds(onDelay);
+            light.enabled = true;
+
+            float onTime = Random.Range(minOnTime, maxOnTime);
+            yield return new WaitForSeconds(onTime);
+            light.enabled = false;
+        }
     }
 }
